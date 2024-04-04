340d773094b33b009f45d664328add593bac935f
S-dev-coder <sk7137389@gmail.com>
2024-04-04 15:19:25 +0530

    Fix formatting in CreateClientFeedbackDto.cs

--- Promact.CustomerSuccess.Platform/Services/Dtos/ClientFeedbacks/CreateClientFeedbackDto.cs
+++ Promact.CustomerSuccess.Platform/Services/Dtos/ClientFeedbacks/CreateClientFeedbackDto.cs
@@ -5,17 +5,11 @@ namespace Promact.CustomerSuccess.Platform.Services.Dtos.ClientFeedbacks
     public class CreateClientFeedbackDto
     {
         public Guid ProjectId { get; set; }
-
-
         public DateTime DateReceived { get; set; } = DateTime.Now;
         public FeedbackType FeedbackType { get; set; }
         public required string DetailedFeedback { get; set; }
         public bool ActionTaken { get; set; }
         public DateTime? ClosureDate { get; set; }
-
-
-
-
     }
 
 }
