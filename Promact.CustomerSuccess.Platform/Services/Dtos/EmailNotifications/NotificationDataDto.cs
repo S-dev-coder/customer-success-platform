4160b7917cba81888da1d79ba294d8643ceb252f
S-dev-coder <sk7137389@gmail.com>
2024-04-05 00:07:00 +0530

    Add email service

--- Promact.CustomerSuccess.Platform/Services/Dtos/EmailNotifications/NotificationDataDto.cs
+++ Promact.CustomerSuccess.Platform/Services/Dtos/EmailNotifications/NotificationDataDto.cs
@@ -4,7 +4,7 @@ namespace Promact.CustomerSuccess.Platform.Services.Dtos.EmailNotifications
 {
     public class NotificationDataDto 
     {
-        public string Subject { get; set; }
-        public string Message { get; set; }
+        public required string Subject { get; set; }
+        public required string Message { get; set; }
     }
 }
